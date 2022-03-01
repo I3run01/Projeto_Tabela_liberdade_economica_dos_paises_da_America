e8cd8813c27cc55488f44dc687a1761ff40495ef
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-03-01 11:48:00 -0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function elementos(Nome,GlobalRanking, LiberdadeEco, IDH, Desemprego) {
     return {
         Nome: Nome,
@@ -41,6 +42,50 @@ function expandir(expan, txt, resp ) {
     }   
 }
 
+function mediaGlobal(lista) {
+
+    var soma = lista.reduce((acumulador, obj) => {
+        acumulador = acumulador + obj.GlobalRanking
+        return acumulador
+    }, 0)
+    var mediaglobal = soma/lista.length
+
+    return mediaglobal
+}
+
+function libEco(lista) {
+
+    var soma = lista.reduce((acumulador, obj) => {
+        acumulador = acumulador + obj.LiberdadeEco
+        return acumulador
+    }, 0)
+    var mediaglobal = soma/lista.length
+
+    return mediaglobal
+}
+
+function IDHF(lista) {
+
+    var soma = lista.reduce((acumulador, obj) => {
+        acumulador = acumulador + obj.IDH
+        return acumulador
+    }, 0)
+    var mediaglobal = soma/lista.length
+
+    return mediaglobal
+}
+
+function TaxDes(lista) {
+
+    var soma = lista.reduce((acumulador, obj) => {
+        acumulador = acumulador + obj.Desemprego
+        return acumulador
+    }, 0)
+    var mediaglobal = soma/lista.length
+
+    return mediaglobal
+}
+
 
 //Países
 Canada = elementos('Canadá',15, 76.6, 0.914, 8.2)
@@ -55,7 +100,7 @@ ElSalvador = elementos('El Salvador', 90, 59.6, 0.659,  4.5)
 Haiti = elementos('Haiti', 145, 50, 0.404, 40)
 Honduras = elementos('Honduras', 92, 59.5, 0.667, 6.2)
 Jamaica = elementos('Jamaica', 46, 67.4, 0.688, 8.50)
-Panama = elementos('Panamá', 56, 65.4, 0.755 )
+Panama = elementos('Panamá', 56, 65.4, 0.755, 6.1 )
 
 Argentina = elementos('Argentina',144, 50.1, 0.775, 9.60)
 Brasil = elementos('Brasil', 133, 53.3, 0.710,  11.11 )
@@ -75,31 +120,76 @@ Paraguai = elementos('Paraguai', 73, 62.9, 0.640, 6.63, 8)
 //Países da América do Norte
 paisesAN = [Canada, EUA, Mexico]
 respAN = estrutura(paisesAN)
+const paisesANOB = {
+    mediaGlobal: mediaGlobal(paisesAN),
+    LiberdadeEco: libEco(paisesAN),
+    IDHO: IDHF(paisesAN),
+    TD: TaxDes(paisesAN)
+}
+
 
 
 paisesAC = [Barbados,CostaRica, RepDom, ElSalvador, Haiti, Honduras, Jamaica, Panama]
 respAC = estrutura(paisesAC)
+const paisesACOB = {
+    mediaGlobal: mediaGlobal(paisesAC),
+    LiberdadeEco: libEco(paisesAC),
+    IDHO: IDHF(paisesAC),
+    TD: TaxDes(paisesAC)
+}
 
 paisesAS = [Argentina, Brasil, Colombia, Bolivia, Venezuela, Chile, Equador, Peru, Suriname, Paraguai]
-respAS = estrutura(paisesAS) 
+respAS = estrutura(paisesAS)
+const paisesASOB = {
+    mediaGlobal: mediaGlobal(paisesAS),
+    LiberdadeEco: libEco(paisesAS),
+    IDHO: IDHF(paisesAS),
+    TD: TaxDes(paisesAS)
+}
 
+//Elementos do HTML
 var textoAN = document.getElementById('linhaAN')
 var expan01 = document.getElementById('expan01')
+var titulo01 = document.getElementById('')
+var d1norte = document.getElementById('d1norte')
+var d2norte = document.getElementById('d2norte')
+var d3norte = document.getElementById('d3norte')
+var d4norte = document.getElementById('d4norte')
 expan01.addEventListener('click', expandirAN)
 function expandirAN() {
     expandir(expan01, textoAN, respAN)
 }
+d1norte.innerHTML = paisesANOB.mediaGlobal
+d2norte.innerHTML = paisesANOB.LiberdadeEco
+d3norte.innerHTML = paisesANOB.IDHO
+d4norte.innerHTML = paisesANOB.TD
 
 var textoAC = document.getElementById('linhaAC')
 var expan02 = document.getElementById('expan02')
+var d1centro = document.getElementById('d1centro')
+var d2centro = document.getElementById('d2centro')
+var d3centro = document.getElementById('d3centro')
+var d4centro = document.getElementById('d4centro')
 expan02.addEventListener('click', expandirAC)
 function expandirAC() {
     expandir(expan02, textoAC, respAC)
 }
+d1centro.innerHTML = paisesACOB.mediaGlobal
+d2centro.innerHTML = paisesACOB.LiberdadeEco
+d3centro.innerHTML = paisesACOB.IDHO
+d4centro.innerHTML = paisesACOB.TD
 
 var textoAS = document.getElementById('linhaAS')
 var expan03 = document.getElementById('expan03')
+var d1sul = document.getElementById('d1sul')
+var d2sul = document.getElementById('d2sul')
+var d3sul = document.getElementById('d3sul')
+var d4sul = document.getElementById('d4sul')
 expan03.addEventListener('click', expandirAS)
 function expandirAS() {
     expandir(expan03, textoAS, respAS)
 }
+d1sul.innerHTML = paisesASOB.mediaGlobal
+d2sul.innerHTML = paisesASOB.LiberdadeEco
+d3sul.innerHTML = paisesASOB.IDHO
+d4sul.innerHTML = paisesASOB.TD
