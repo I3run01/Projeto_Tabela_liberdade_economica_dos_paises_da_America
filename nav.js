f713e14b8c1fdfddb6de1354393be3bdc7780755
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-03-03 16:06:35 -0300

    commit

--- nav.js
+++ nav.js
@@ -1,9 +1,17 @@
 var navbar  = document.getElementById('mobile')
 var menubar = document.getElementById('menu')
+var sair = document.getElementById('sair')
 
 navbar.addEventListener('click', ShowMenu)
+sair.addEventListener('click', SairMenu)
 
 function ShowMenu() {
     menubar.style.display = 'block'
     
 }
+
+function SairMenu() {
+    menubar.style.display = 'none'
+
+}
+
